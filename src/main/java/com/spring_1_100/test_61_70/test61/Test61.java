b1f1ea26d2fe2dfaffde43a4d6b31ac406f68a64
quyixiao <2621048238@qq.com>
2020-06-23 18:08:30 +0800

    提交修改

--- src/main/java/com/spring_1_100/test_61_70/test61/Test61.java
+++ src/main/java/com/spring_1_100/test_61_70/test61/Test61.java
@@ -11,7 +11,7 @@ public class Test61 {
     public static void main(String[] args) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring_1_100/config_61_70/spring61.xml");
 
-        Object[] params = {"John", new GregorianCalendar().getTime()};
+        Object[] params = {"test", new GregorianCalendar().getTime()};
 
         String str1 = ac.getMessage("test", params, Locale.US);
         String str2 = ac.getMessage("test", params, Locale.CHINA);
