ab9692edee5507cb51122e0a2e20fac113075b78
quyixiao <2621048238@qq.com>
2020-08-31 11:08:33 +0800

    提交修改

---
+++ src/main/java/com/spring_101_200/test_101_110/test109_ignore_depency/SpringContextUtils.java
@@ -0,0 +1,20 @@
+package com.spring_101_200.test_101_110.test109_ignore_depency;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringContextUtils implements ApplicationContextAware {
+    public  ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    public ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+}
---
+++ src/main/java/com/spring_101_200/test_101_110/test109_ignore_depency/UserController.java
@@ -0,0 +1,19 @@
+package com.spring_101_200.test_101_110.test109_ignore_depency;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserController {
+
+    @Autowired
+    private SpringContextUtils springContextUtils;
+
+
+    public void test() {
+        UserService userService = springContextUtils.getApplicationContext().getBean(UserService.class);
+        userService.get();
+    }
+
+
+}
