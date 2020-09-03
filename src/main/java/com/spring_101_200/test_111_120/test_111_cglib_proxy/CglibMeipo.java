bbab63665a1e179c46af1d048d3a39df743a319e
quyixiao <2621048238@qq.com>
2020-09-03 18:41:22 +0800

    提交修改

--- src/main/java/com/spring_101_200/test_111_120/test_111_cglib_proxy/CglibMeipo.java
+++ src/main/java/com/spring_101_200/test_111_120/test_111_cglib_proxy/CglibMeipo.java
@@ -21,5 +21,4 @@ public class CglibMeipo implements MethodInterceptor {
         return obj;
     }
 
-
 }
--- src/main/java/com/spring_101_200/test_111_120/test_111_cglib_proxy/Customer.java
+++ src/main/java/com/spring_101_200/test_111_120/test_111_cglib_proxy/Customer.java
@@ -1,8 +1,6 @@
 package com.spring_101_200.test_111_120.test_111_cglib_proxy;
 
 public class Customer {
-
-
     public void findLove() {
         System.out.println("肤白貌美大长腿");
     }
