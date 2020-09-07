95858271021d2490042700a97a51692c678ae5b1
quyixiao <2621048238@qq.com>
2020-09-07 09:13:21 +0800

    提交修改

--- src/main/java/com/spring_1_100/test_41_50/test41/User.java
+++ src/main/java/com/spring_1_100/test_41_50/test41/User.java
@@ -1,5 +1,8 @@
 package com.spring_1_100.test_41_50.test41;
 
+import lombok.Data;
+
+@Data
 public class User {
 
     private String userName;
@@ -8,17 +11,18 @@ public class User {
 
     private Integer sex;
 
-    public User() {
+
+    public User(Integer age, Integer sex) {
+        this.age = age;
+        this.sex = sex;
     }
+
     public User(String userName, Integer age) {
         this.userName = userName;
         this.age = age;
     }
 
-
-    public User(Integer age, Integer sex) {
-        this.age = age;
-        this.sex = sex;
+    public User() {
     }
 
     public User(String userName, Integer age, Integer sex) {
@@ -27,27 +31,4 @@ public class User {
         this.sex = sex;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
-    }
 }
