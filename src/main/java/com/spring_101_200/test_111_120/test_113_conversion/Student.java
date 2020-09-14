d76602c0e9f96bedb0fd623776f8482ed9c555c5
quyixiao <2621048238@qq.com>
2020-09-14 09:23:48 +0800

    提交修改

---
+++ src/main/java/com/spring_101_200/test_111_120/test_113_conversion/Student.java
@@ -0,0 +1,15 @@
+package com.spring_101_200.test_111_120.test_113_conversion;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String score;
+
+    private User user;
+
+
+
+
+}
---
+++ src/main/java/com/spring_101_200/test_111_120/test_113_conversion/User.java
@@ -0,0 +1,9 @@
+package com.spring_101_200.test_111_120.test_113_conversion;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String realName;
+    private int age ;
+}
