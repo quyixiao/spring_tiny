def5ff6b87d4b390d4cebf6967f93961c25f8697
quyixiao <2621048238@qq.com>
2020-09-16 11:50:54 +0800

    提交候tgit pull

--- src/main/java/com/spring_101_200/test_111_120/test_113_conversion/spel/TestSpel.java
+++ src/main/java/com/spring_101_200/test_111_120/test_113_conversion/spel/TestSpel.java
@@ -20,9 +20,7 @@ public class TestSpel {
 
     @Test
     public void test11_math_2() {
-        // -- NOT --
         ExpressionParser parser = new SpelExpressionParser();
-        // Addition
         int two = parser.parseExpression("2 + 3  * 4").getValue(Integer.class); // 2
         System.out.println(two);
     }
@@ -30,9 +28,7 @@ public class TestSpel {
 
     @Test
     public void test11_math_1() {
-        // -- NOT --
         ExpressionParser parser = new SpelExpressionParser();
-        // Addition
         int two = parser.parseExpression("(21 + 3) * 4").getValue(Integer.class); // 2
         //int two = parser.parseExpression("2 + 3 + 4").getValue(Integer.class); // 2
         System.out.println(two);
