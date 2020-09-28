9cf4e0db8fc3b37af9eea0e419c7ce9a67ec9855
quyixiao <2621048238@qq.com>
2020-09-28 09:13:40 +0800

    提交修改

--- src/main/java/com/spring_1_100/test_41_50/test50/Test50.java
+++ src/main/java/com/spring_1_100/test_41_50/test50/Test50.java
@@ -1,18 +1,16 @@
 package com.spring_1_100.test_41_50.test50;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
-public class SpringFactoryMethod {
+public class Test50 {
     public static void main(String[] args) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring_1_100/config_41_50/spring50.xml");
-
-
         //获取动态Bean
-        //Stu dynamicStu = cast(ac.getBean("dynamicStu"));
-
-        //System.out.println(dynamicStu);
+        Stu stu =(Stu) ac.getBean("dynamicStu");
+        System.out.println(JSON.toJSONString(stu));
 
     }
 
