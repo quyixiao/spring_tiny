27a7856d8e2ff44fa24671a8a7cb8639aaaf4dfa
quyixiao <2621048238@qq.com>
2020-09-28 17:50:27 +0800

    提交修改

--- src/main/java/com/spring_101_200/test_111_120/test_115_custom_label/UserBeanDefinitionParser.java
+++ src/main/java/com/spring_101_200/test_111_120/test_115_custom_label/UserBeanDefinitionParser.java
@@ -3,6 +3,7 @@ package com.spring_101_200.test_111_120.test_115_custom_label;
 import com.utils.StringUtils;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSimpleBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
 import org.w3c.dom.Element;
 
 public class UserBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser {
@@ -14,15 +15,16 @@ public class UserBeanDefinitionParser extends AbstractSimpleBeanDefinitionParser
     }
 
     // 从 element 中解析并提取出对应的元素
-    protected  void doParse(Element element, BeanDefinitionBuilder bean){
+    @Override
+    protected void doParse(Element element, ParserContext parserContext, BeanDefinitionBuilder builder) {
         String userName = element.getAttribute("userName");
         String email = element.getAttribute("email");
         //将提取的数据放到 BeanDefinitionBuilder 中，待完成所有的 bean 注册后，统一注册到 beanFactory 中
         if(StringUtils.isNotBlank(userName)){
-            bean.addPropertyValue("userName",userName);
+            builder.addPropertyValue("userName", userName);
         }
         if(StringUtils.isNotBlank(email)){
-            bean.addPropertyValue("email",email);
+            builder.addPropertyValue("email",email);
         }
     }
 
