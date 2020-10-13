7cc565389f7ea76709b4167a0090526ac9f7ee7b
quyixiao <2621048238@qq.com>
2020-10-13 14:43:10 +0800

    提交修改

--- src/main/java/com/spring_1_100/test_31_40/test35_resource_inject/Test35.java
+++ src/main/java/com/spring_1_100/test_31_40/test35_resource_inject/Test35.java
@@ -1,6 +1,8 @@
 package com.spring_1_100.test_31_40.test35_resource_inject;
 
 
+import com.spring_1_100.test_31_40.test35_resource_inject.anno.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,11 +10,22 @@ public class Test35 {
     // 这时，如果@Resource未指定"car"属性，则也可以根据属性方法得到需要注入的Bean的名称，可见@Autowired 默认按类型
     // 匹配注入Bean 的，只不过它没有required属性，可见不管是@resource还是@Inject注解，其功能都没有
     // @Autowired丰富，因此除非必要在可以不必在乎这两个属性
-    public static void main(String[] args) {
+    @Test
+    public void test() {
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring_1_100/config_31_40/spring35_resource_inject.xml");
         Boss boss = (Boss) ac.getBean("boss");
         System.out.println(boss.getCar());
     }
+
+
+    public static void main(String[] args) {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring_1_100/config_31_40/spring35_resource_inject_1.xml");
+        User user = (User) ac.getBean("xxxx");
+        System.out.println(user);
+    }
+
+
+
 }
 
 
---
+++ src/main/java/com/spring_1_100/test_31_40/test35_resource_inject/anno/User.java
@@ -0,0 +1,9 @@
+package com.spring_1_100.test_31_40.test35_resource_inject.anno;
+
+import org.springframework.stereotype.Service;
+
+@Service("xxxx")
+@MyTestAnnotation("/user/info")
+public class User {
+
+}
