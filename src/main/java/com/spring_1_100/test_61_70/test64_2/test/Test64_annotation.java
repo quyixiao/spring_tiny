34f6f349d0594cc400c848fd09615bac9ead1514
quyixiao <2621048238@qq.com>
2020-10-20 16:04:01 +0800

    提交修改

--- src/main/java/com/spring_1_100/test_61_70/test64_2/test/Test64_annotation.java
+++ src/main/java/com/spring_1_100/test_61_70/test64_2/test/Test64_annotation.java
@@ -1,7 +1,6 @@
 package com.spring_1_100.test_61_70.test64_2.test;
 
 import com.spring_1_100.test_61_70.test64_2.MyService;
-import com.spring_1_100.test_61_70.test64_2.StaffService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -9,13 +8,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Test64_annotation {
 
     @Test
-    public void annotation1(){
+    public void annotation1() {
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring_1_100/config_61_70/spring64_2.xml");
-        System.out.println("===========================");
-        StaffService staffService = ac.getBean(StaffService.class);
-        staffService.getStaffInfo();
-
-        System.out.println("--------------------------------");
 
         MyService myService = ac.getBean(MyService.class);
         myService.select();
