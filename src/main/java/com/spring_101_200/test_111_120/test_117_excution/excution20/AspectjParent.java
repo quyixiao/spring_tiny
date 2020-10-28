238ebb603bf8fb810331b8c164426625e695fd6a
quyixiao <2621048238@qq.com>
2020-10-28 10:55:23 +0800

    提交修改

--- src/main/java/com/spring_101_200/test_111_120/test_117_excution/excution20/AspectjParent.java
+++ src/main/java/com/spring_101_200/test_111_120/test_117_excution/excution20/AspectjParent.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.annotation.Pointcut;
 
 public class AspectjParent  extends AspectjParentParent{
 
-    @Pointcut("execution(* com..MySer*.*(..))")                                                    //com 包及所有子包下UserService接口中的任何无参方法
+    @Pointcut("execution(* com..MySerxx*.*(..))")                                                    //com 包及所有子包下UserService接口中的任何无参方法
     public void test1() {
 
     }
