92ce8593b0c4248c88526cd2ebaaf78dd819e273
quyixiao <2621048238@qq.com>
2020-11-20 18:49:18 +0800

    提交修改

---
+++ src/main/java/com/spring_101_200/test_131_140/test_132_mybatis_typehandlers/User.java
@@ -0,0 +1,24 @@
+package com.spring_101_200.test_131_140.test_132_mybatis_typehandlers;
+
+import lombok.Data;
+import org.apache.ibatis.builder.BuilderException;
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
+import org.apache.ibatis.executor.ErrorContext;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.parsing.XNode;
+
+import java.io.InputStream;
+import java.util.Date;
+
+@Data
+public class User {
+    private Long id;
+    private Integer isDelete;
+    private Date gmtCreate;
+    private Date gmtModified;
+    private String username;
+    private String password;
+    private PhoneNumber realName;
+    private Long managerId;
+
+}
---
+++ src/main/java/com/spring_101_200/test_131_140/test_132_mybatis_typehandlers/UserMapper.java
@@ -0,0 +1,9 @@
+package com.spring_101_200.test_131_140.test_132_mybatis_typehandlers;
+
+public interface UserMapper {
+
+    void insertUser(User user);
+
+   User getUser(Long id);
+
+}
