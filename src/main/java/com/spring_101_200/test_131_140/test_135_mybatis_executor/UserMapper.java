a4fd0dda1c5c3cb579ae3da292ba408e643045eb
quyixiao <2621048238@qq.com>
2020-11-24 16:18:06 +0800

    提交修改

--- src/main/java/com/spring_101_200/test_131_140/test_135_mybatis_executor/UserMapper.java
+++ src/main/java/com/spring_101_200/test_131_140/test_135_mybatis_executor/UserMapper.java
@@ -2,6 +2,8 @@ package com.spring_101_200.test_131_140.test_135_mybatis_executor;
 
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 public interface UserMapper {
 
     User findUserById(@Param("id") Long id);
@@ -9,4 +11,5 @@ public interface UserMapper {
     int update(@Param("id") long id);
 
     void updateRealName(@Param("id") long id, @Param("realName") String realName);
+
 }
--- src/main/java/com/spring_101_200/test_131_140/test_136_mybatis_saferowboundsenabled/Test136.java
+++ src/main/java/com/spring_101_200/test_131_140/test_136_mybatis_saferowboundsenabled/Test136.java
@@ -1,51 +1,47 @@
-package com.spring_101_200.test_131_140.test_135_mybatis_executor;
+package com.spring_101_200.test_131_140.test_136_mybatis_saferowboundsenabled;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 
 import java.util.List;
 
-public class Test135 {
+public class Test136 {
 
 
-    //https://blog.csdn.net/weixin_34267123/article/details/92407774
     static SqlSessionFactory sqlSessionFactory = null;
 
     static {
         sqlSessionFactory = MyBatisUtil.getSqlSEssionFactory();
     }
 
+
     @Test
-    public void test1() throws Exception {
+    public void testGetUser() throws Exception {
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        User user = userMapper.findUserById(456l);
-        user = userMapper.findUserById(456l);
-        System.out.println(JSON.toJSONString(user));
+        List<UserBillInfo> aa = sqlSession.selectList("com.spring_101_200.test_131_140.test_136_mybatis_saferowboundsenabled.UserMapper.selectUserBill",
+                null, new RowBounds(0, 5));
+        System.out.println(JSON.toJSONString(aa));
     }
+
 
 
     @Test
     public void test2() throws Exception {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        userMapper.updateRealName(456l,"zhangsan");
-        userMapper.updateRealName(457l,"lisi");
+        UserBillInfo userBillInfo = userMapper.selectUserBillResultType(456l);
+        System.out.println(JSON.toJSONString(userBillInfo));
     }
-
 
 
     @Test
     public void test3() throws Exception {
         SqlSession sqlSession = sqlSessionFactory.openSession();
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
-        userMapper.updateRealName(456l,"zhangsan");
-        userMapper.update(458l);
-        userMapper.updateRealName(457l,"lisi");
+        UserBillInfo userBillInfo = userMapper.selectUserBill(456l,new RowBounds(0,5));
+        System.out.println(JSON.toJSONString(userBillInfo));
     }
-
-
-
 }
