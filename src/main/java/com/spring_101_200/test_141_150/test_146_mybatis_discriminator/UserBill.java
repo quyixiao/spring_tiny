5b2d18d9a7141c7bc5afb9ac35edb5d757a2f7e5
quyixiao <2621048238@qq.com>
2020-11-25 17:37:05 +0800

    提交修改

---
+++ src/main/java/com/spring_101_200/test_141_150/test_146_mybatis_discriminator/UserBill.java
@@ -0,0 +1,16 @@
+package com.spring_101_200.test_141_150.test_146_mybatis_discriminator;
+
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class UserBill {
+    private Long id;
+    private String type;
+    private Long userId;
+    private BigDecimal amount;
+    private User user;
+
+}
