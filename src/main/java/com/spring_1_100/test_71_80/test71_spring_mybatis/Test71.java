15548243ae70b8cb108ed39ea15c4a454d76269c
quyixiao <2621048238@qq.com>
2020-12-15 19:42:41 +0800

    提交候tgit pull

--- src/main/java/com/spring_1_100/test_71_80/test71_spring_mybatis/Test71.java
+++ src/main/java/com/spring_1_100/test_71_80/test71_spring_mybatis/Test71.java
@@ -6,15 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test71 {
     public static void main(String[] args) {
-
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring_1_100/config_71_80/spring71_mybatis_origin/spring71.xml");
-
-
         UserMapper userMapper = (UserMapper) ac.getBean("userMapper");
         User user = userMapper.getUser(456l);
         System.out.println(JSON.toJSONString(user));
-
-
     }
 
 }
