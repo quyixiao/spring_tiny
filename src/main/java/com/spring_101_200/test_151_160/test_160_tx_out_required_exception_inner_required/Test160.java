d90a7662a66bd503f200da893069f45695731e21
quyixiao <2621048238@qq.com>
2020-12-17 10:50:23 +0800

    提交修改

---
+++ src/main/java/com/spring_101_200/test_151_160/test_160_tx_out_required_exception_inner_required/Test160.java
@@ -0,0 +1,22 @@
+package com.spring_101_200.test_151_160.test_160_tx_out_required_exception_inner_required;
+
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+// https://blog.csdn.net/yanyan19880509/article/details/53041564/
+public class Test160 {
+
+
+
+    //6========[required-exception[required]]=====result:[rollback[rollback]]=========================================
+    @Test
+    public void updateUserRequiresTest2() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("spring_101_200/config_151_160/spring160.xml");
+        TxService txService = (TxService) ac.getBean("txService");
+        txService.updateUserRequiresTest2();
+    }
+
+
+}
