16a3922066b52a1e9bd154997378b2ad385911db
quyixiao <2621048238@qq.com>
2021-01-07 09:45:07 +0800

    提交修改

--- src/main/java/com/spring_101_200/test_181_190/test_185_spring_mvc/User.java
+++ src/main/java/com/spring_101_200/test_181_190/test_185_spring_mvc/User.java
@@ -1,10 +1,6 @@
 package com.spring_101_200.test_181_190.test_185_spring_mvc;
 
-import lombok.Data;
-
-@Data
 public class User {
-
 
     private String username;
 
@@ -12,6 +8,30 @@ public class User {
 
     private UserInfo userInfo;
 
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserInfo getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfo userInfo) {
+        this.userInfo = userInfo;
+    }
 }
 
 
--- src/main/java/com/spring_101_200/test_191_200/test_196_spring_mvc_ignore_default_model_on_redirect/TestController.java
+++ src/main/java/com/spring_101_200/test_191_200/test_196_spring_mvc_ignore_default_model_on_redirect/TestController.java
@@ -31,6 +31,7 @@ public class TestController {
         } else {
             return "error";
         }
+
     }
 
 
